30660300fb04c32c9685703d08cc5546069bf071
Jose Manuel Lopez <chepemlg@hotmail.com>
2026-03-04 14:21:47 -0500

    Idea Cards

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
+Route::redirect('/', '/ideas');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/ideas', [IdeaController::class, 'index'])->name('idea.index');
+    Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show');
+});
+
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create'])->name('register.create');
     Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
-    Route::get('/login', [SessionsController::class, 'create'])->name('login.create');
+    Route::get('/login', [SessionsController::class, 'create'])->name('login');
     Route::post('/login', [SessionsController::class, 'store'])->name('login.store');
 });
 
